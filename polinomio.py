320393d1b72adbed9702d8aad71860560a0a1f61
Ruth Maria Rodriguez Simon <rrs999@inlumine.ual.es>
2022-04-21 19:26:10 +0200

    Version 3.3

--- polinomio.py
+++ polinomio.py
@@ -3,11 +3,13 @@ class calc:
 
     @classmethod
     def suma(cls, x, y):
-        '''argumentos de entrada: dos listas, cada una con un polinomio
+        '''
+        argumentos de entrada: dos listas, cada una con un polinomio
         retorna: la suma de los dos polinomios
         '''
         may = x
         men = y
+        sum = [0]*len(may)
         if len(x) < len(y):
             men = x
             may = y
@@ -21,11 +23,13 @@ class calc:
 
     @classmethod
     def resta(cls, x, y):
-        '''argumentos de entrada: dos listas, cada una con un polinomio
+        '''
+        argumentos de entrada: dos listas, cada una con un polinomio
         retorna: la resta de los dos polinomios
         '''
         may = x
         men = [y[i]*(-1) for i in range(len(y))]
+        sum = [0]*len(may)
         if len(x) < len(y):
             men = x
             may = [y[i]*(-1) for i in range(len(y))]
